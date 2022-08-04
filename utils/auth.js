7dcd80502c233af7e38f5d7435d5cdfe89d7957f
Anne McCorkle <annefmccorkle@gmail.com>
2022-08-03 21:03:06 -0600

    updating the package.json with start, seeds, and nodemon

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,6 @@
 const withAuth = (req, res, next) => {
     //checks to see if the user is authenticated, if not redirects them to the login page
-    if (!req.session.loggedIn) {
+    if (!req.session.logged_in) {
         res.redirect('/login');
     } else {
         next();
