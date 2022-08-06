f0ebbe889d3d4efba34c308b5919291cc601ec2a
Anne McCorkle <annefmccorkle@gmail.com>
2022-08-06 09:55:56 -0600

    working login api fetch

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -10,6 +10,6 @@ const userData = [
   },
 ];
 
-const seedUsers = () => User.bulkCreate(userData);
+const seedUsers = () => User.bulkCreate(userData, { individualHooks: true });
 
 module.exports = seedUsers;
