6f37cd74855fd8668551ab15385a2b76bde7d2de
Anne McCorkle <annefmccorkle@gmail.com>
2022-08-08 15:03:41 -0600

    updating nodemon start, reinstalling npm for nodemon

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,18 +1,13 @@
 // sign up with us
 const signupFormHandler = async(event) => {
     event.preventDefault();
-
-    console.log("hello world!");
     
     //collect data from the form
-    //no id's here yet for first or last name, do we need them?
     const lastName = document.querySelector("#last-name").value.trim();
     const firstName = document.querySelector("#first-name").value.trim();
     const email = document.querySelector("#email-signup").value.trim();
     const password = document.querySelector("#password-signup").value.trim();
     
-    console.log(lastName, firstName, email, password);
-
     if (email && password) {
         //send data to the server
         const response = await fetch ("/api/users/signup", {
@@ -22,7 +17,8 @@ const signupFormHandler = async(event) => {
         });
         if(response.ok) {
             //then redirect browser to the food choices home page
-            document.location.replace("/dashboard")
+            alert("Thank you for signing up!");
+            document.location.replace("/dashboard");
         } else {
             alert("Your login was unsuccessful, please try again");
         }
