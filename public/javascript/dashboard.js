06bc4c85761a86bd55e15e465e13e510f3f19f23
Brisco Arechederra <brisco.arch@gmail.com>
2022-08-09 14:05:53 -0600

    added files

---
+++ public/javascript/dashboard.js
@@ -0,0 +1,23 @@
+function updateUserPrefs() {
+  //collect values from the user modal
+  const cuisines = document.getElementsByClassName("cuisine");
+
+  console.log(cuisines);
+
+  // if (email && password) {
+  //     //send the email and password to the server with a post request
+  //     const response = await fetch ("/api/users/login", {
+  //         method: "POST",
+  //         body: JSON.stringify({ email, password }),
+  //         headers: { "Content-Type": "application/json"},
+  //     });
+  //     console.log(response);
+  //     if(response.ok) {
+  //         //then redirect browser to the food choices home page
+  //         document.location.replace("/dashboard")
+  //     } else {
+  //         alert("Your login was unsuccessful, please try again");
+  //     }
+  //     console.log(response);
+  // }
+}
