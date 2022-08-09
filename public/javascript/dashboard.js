2d0a15a567b59ee32e507e0fa59132d25b5aff09
Brisco Arechederra <brisco.arch@gmail.com>
2022-08-09 16:00:06 -0600

    added user input functions

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -1,8 +1,53 @@
 function updateUserPrefs() {
   //collect values from the user modal
-  const cuisines = document.getElementsByClassName("cuisine");
+  //   diet
+  const temp_diet = [...document.getElementsByClassName("diet")];
+  let diet = "";
 
+  temp_diet.forEach((element) => {
+    if (element.selected) {
+      console.log(element);
+      diet = element.value;
+    }
+  });
+
+  // intolerances
+  const temp_intolerances = [...document.getElementsByClassName("int")];
+  const intolerances = [];
+  temp_intolerances.forEach((element) => {
+    if (element.ariaPressed) {
+      intolerances.push(element.value);
+    }
+  });
+  // cuisines
+  const temp_cuisines = [...document.getElementsByClassName("cuisine")];
+  const cuisines = [];
+  temp_cuisines.forEach((element) => {
+    if (element.ariaPressed) {
+      cuisines.push(element.value);
+    }
+  });
+  console.log("diet");
+  console.log(diet);
+  console.log("intolerances");
+  console.log(intolerances);
+  console.log("cuisines");
   console.log(cuisines);
+
+  function inputToQuery(array) {
+    return array.join("+");
+  }
+
+  const query_intolerances = inputToQuery(intolerances);
+  const query_cuisines = inputToQuery(cuisines);
+
+  console.log("After Consolidation:");
+  console.log("diet");
+  console.log(diet);
+  console.log("intolerances");
+  console.log(query_intolerances);
+  console.log("cuisines");
+  console.log(query_cuisines);
 
   // if (email && password) {
   //     //send the email and password to the server with a post request
