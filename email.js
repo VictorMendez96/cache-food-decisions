25d23fbd3fba21e223e840d02affb79338103ce5
deftonechris <deftonechris@msn.com>
2022-08-10 11:26:21 -0600

    i didnt change anything but formating here

--- email.js
+++ email.js
@@ -13,7 +13,6 @@ app.set("view engine", "handlebars");
 
 app.use("/public", express.static(path.join(__dirname, "public")));
 
-
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -22,44 +21,42 @@ app.get("/", (req, res) => {
   res.render("email");
 });
 
+app.post("/send", (req, res) => {
+  let e = req.body.e;
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
 
-app.post('/send', (req, res) => {
-  let e = req.body.e
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
-  },
-});
-
-const handlebarOptions = {
-  viewEngine: {
+  const handlebarOptions = {
+    viewEngine: {
+      extName: ".handlebars",
+      partialsDir: path.resolve(__dirname, "views"),
+      defaultLayout: false,
+    },
+    viewPath: path.resolve(__dirname, "views"),
     extName: ".handlebars",
-    partialsDir: path.resolve(__dirname, "views"),
-    defaultLayout: false,
-  },
-  viewPath: path.resolve(__dirname, "views"),
-  extName: ".handlebars",
-};
+  };
 
-transporter.use("compile", hbs(handlebarOptions));
+  transporter.use("compile", hbs(handlebarOptions));
 
-const mailOptions = {
-  from: "cachefooddecisions@gmail.com",
-  to: e,
-  subject: "Invoices due",
-  text: "Dudes, we really need your money.",
-  template: "email",
-};
+  const mailOptions = {
+    from: "cachefooddecisions@gmail.com",
+    to: e,
+    subject: "Invoices due",
+    text: "Dudes, we really need your money.",
+    template: "email",
+  };
 
-transporter.sendMail(mailOptions, function (error, info) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Email sent: " + info.response);
-  }
-});
-
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
 });
 app.listen(3000, () => console.log("Server has started..."));
