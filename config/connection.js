9863315c94609b0497df66fb5a7a904be3d4c314
Anne McCorkle <annefmccorkle@gmail.com>
2022-08-12 15:57:44 -0600

    recofiguring the config file for heroku

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,8 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: "process.env.HOST",
+      // host: "process.env.HOST",
+      host: "localhost",
       dialect: "mysql",
       port: 3306,
     }
