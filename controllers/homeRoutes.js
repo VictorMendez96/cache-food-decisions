e51dafdcab093c77c96694d3a6f8701a78a89dba
Brisco Arechederra <brisco.arch@gmail.com>
2022-08-12 15:52:06 -0600

    recipe fetch and render comeplete

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { User } = require("../models");
 const withAuth = require("../utils/auth");
+const { getChoices } = require("../utils/query");
 
 // prevent non logged in users from viewing the dashboard
 router.get("/dashboard", withAuth, async (req, res) => {
@@ -43,13 +44,13 @@ router.get("/recipes", withAuth, async (req, res) => {
     //find the user based on email
     const userData = await User.findAll({
       attributes: { exclude: ["password"] },
+      where: { id: req.session.user_id },
     });
+    //get
+    let recipes = await getChoices(userData[0].dataValues);
 
-    //serialize the data
-    const users = userData.map((user) => user.get({ plain: true }));
-    // Pass the logged in flag to the template
     res.render("recipe", {
-      users,
+      recipes,
       logged_in: req.session.logged_in,
     });
   } catch (error) {
@@ -57,7 +58,7 @@ router.get("/recipes", withAuth, async (req, res) => {
   }
 });
 
-router.get("/recipes", withAuth, async (req, res) => {
+router.get("/shoppingList", withAuth, async (req, res) => {
   try {
     //find the user based on email
     const userData = await User.findAll({
@@ -66,9 +67,14 @@ router.get("/recipes", withAuth, async (req, res) => {
 
     //serialize the data
     const users = userData.map((user) => user.get({ plain: true }));
+    console.log("users");
+    console.log(users);
+    console.log("userData");
+    console.log(userData);
     // Pass the logged in flag to the template
     res.render("recipe", {
       users,
+      recipes,
       logged_in: req.session.logged_in,
     });
   } catch (error) {
